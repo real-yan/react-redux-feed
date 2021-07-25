539c155e617c4f1cd16b817a3fa868c2e8248d96
Yan Real <real.yan.yr@gmail.com>
2021-07-25 11:22:05 -0300

    Fetching users from a fakeAPI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import store from './app/store'
 import { Provider } from 'react-redux'
 
+import App from './App'
+import store from './app/store'
+import { fetchUsers } from './features/users/usersSlice'
+
+import './index.css'
 import './api/server'
+
+store.dispatch(fetchUsers())
 
 ReactDOM.render(
   <React.StrictMode>
